e144e955b0564f272679a9a4ea0b1f320747e072
pxai <pello_altadill@cuatrovientos.org>
2016-02-18 00:12:55 +0100

    added hero, weapon and enemy

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Hero : MonoBehaviour {
     private Animator heroAnimator;
+    public GameObject weapon;
+    private float weaponSpeed = 5f;
+    private float fireRate = 0.4f;
+    private Vector3 direction;
 
     // Use this for initialization
     void Start () {
@@ -11,23 +15,19 @@ public class Hero : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //if (heroAnimator.GetCurrentAnimatorStateInfo(0).IsName("Flying"))
-        //{
-        //    status = FLYING_STATUS;
-        //}
 
-        Vector3 direction = new Vector3();
+        direction = new Vector3();
 
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    //Invokes the method methodName in time seconds, 
-        //    //then repeatedly every repeatRate seconds.
-        //    InvokeRepeating("Fire", 0.0001f, firingRate);
-        //}
-        //if (Input.GetKeyUp(KeyCode.Space))
-        //{
-        //    CancelInvoke("Fire");
-        //}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Invokes the method methodName in time seconds, 
+            //then repeatedly every repeatRate seconds.
+            InvokeRepeating("fire", 0.0001f, fireRate);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke("fire");
+        }
 
 
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -57,5 +57,22 @@ public class Hero : MonoBehaviour {
         //float newY = Mathf.Clamp(transform.position.y, 0f, 10f);
         transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         transform.Translate(direction.normalized * 1 * Time.deltaTime);
+    }
+
+
+    void fire()
+    {
+        GameObject throwableWeapon = Instantiate(weapon, transform.position, Quaternion.identity) as GameObject;     
+        throwableWeapon.GetComponent<Rigidbody2D>().velocity = direction.normalized * weaponSpeed;
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+
     }
 }
---
+++ Assets/Sword.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Rotate(Vector3.right, Time.deltaTime);
+        //transform.Rotate(Vector3.up, Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward * -0.9f);
+    }
+}
